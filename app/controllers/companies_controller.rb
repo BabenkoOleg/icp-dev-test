481d4d3ab23717543cec0e8d58882e25b5af9b23
Kravchuk <majerkravchuk@gmail.com>
2019-01-24 11:41:51 +0300

    Add companies controller

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,51 @@
+class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :update, :destroy]
+
+  # GET /companies
+  def index
+    companies = Company.all
+    render json: CompanySerializer.new(companies)
+  end
+
+  # GET /companies/:id
+  def show
+    render json: CompanySerializer.new(@company)
+  end
+
+  # POST /companies
+  def create
+    company = Company.new(company_params)
+
+    if company.save
+      render json: CompanySerializer.new(company)
+    else
+      render json: company.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /companies/:id
+  def update
+    if @company.update(company_params)
+      render json: CompanySerializer.new(@company)
+    else
+      render json: @company.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /companies/1
+  def destroy
+    @company.destroy
+    head :ok
+  end
+
+  private
+
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company)
+          .permit(:name, :email, :phone, :address, :web_site, :contact_person_id)
+  end
+end
---
+++ spec/controllers/companies_controller_spec.rb
@@ -0,0 +1,51 @@
+RSpec.describe CompaniesController, type: :controller do
+  before(:each) { authenticated_header(request) }
+  let(:company) { create(:company) }
+
+  context 'index' do
+    before(:each) { get :index }
+    it { expect(response).to have_http_status(:success) }
+  end
+
+  context 'show' do
+    before(:each) { get :show, params: { id: company.id } }
+    it { expect(response).to have_http_status(:success) }
+  end
+
+  context 'create' do
+    before(:each) { get :create, params: company_params }
+
+    context 'success' do
+      let(:company_params) { { company: build(:company).as_json } }
+
+      it { expect(response).to have_http_status(:success) }
+    end
+
+    context 'unprocessable_entity' do
+      let(:company_params) { { company: build(:company, name: nil).as_json } }
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
+    end
+  end
+
+  context 'update' do
+    before(:each) { put :update, params: company_params }
+
+    context 'success' do
+      let(:company_params) { { id: company.id, company: { name: 'New name' } } }
+
+      it { expect(response).to have_http_status(:success) }
+    end
+
+    context 'unprocessable_entity' do
+      let(:company_params) { { id: company.id, company: { name: nil } } }
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
+    end
+  end
+
+  context 'destroy' do
+    before(:each) { delete :destroy, params: { id: company.id } }
+    it { expect(response).to have_http_status(:success) }
+  end
+end
